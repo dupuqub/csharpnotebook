0ffca6d001cebb1d4b1244dbe5c113eb60811d86
dupuqub <dupuqub@gmail.com>
2018-01-17 04:51:37 -0200

    First commit

---
+++ Script00.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script00 : MonoBehaviour {
+
+	// Esses são alguns tipos de variáveis
+	//
+	int    MeuValor   = 0;
+	float  OutroValor = 12.34f;
+	string FraseJoia  = "badabin badabun";
+	bool   AchoQueSim = true;
+	bool   AchoQueNao = false;
+
+	// Essa é a função de pré-inicialização (específica)
+	// Essa função roda mesmo com o script desabilitado
+	//
+	void Awake () {
+		MeuValor = 500;
+		Debug.Log (MeuValor);
+	}
+
+	// Essa é a função de inicialização (específica)
+	//
+	void Start () {
+		Debug.Log (MeuValor);
+		Debug.Log (MeuValor + OutroValor);
+		Debug.Log (MeuValor + FraseJoia);
+		Debug.Log (AchoQueSim & AchoQueNao);
+	}
+
+	// Essa é a função de repetição (específica)
+	//
+	void Update () {
+		MeuValor ++;
+		Debug.Log (MeuValor);
+	}
+}
---
+++ Script01.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script01 : MonoBehaviour {
+
+	// Funções não podem ser chamadas fora de "Awake", "Start" or "Update"
+	//
+	void Start () {
+
+		NinjaQuieto ();
+
+		Debug.Log (IntBonitao ());
+		Debug.Log (FloatManeiro ());
+		Debug.Log (StringXou ());
+		Debug.Log (BoolHonesto ());
+
+		Debug.Log (Soma (12, 15));
+	}
+
+	// Essa é uma função que não retorna nada
+	//
+	void NinjaQuieto () {Debug.Log ("Jutsu!");}
+
+	// Todas as funções abaixo retornam algo
+	//
+	int    IntBonitao   () {return 3;}
+	float  FloatManeiro () {return 3.141f;}
+	string StringXou    () {return "vixi";}
+	bool   BoolHonesto  () {return false;}
+
+	// A função abaixo mostra como receber parâmetros
+	//
+	float Soma ( float a , float b ) {return a + b;}
+}
