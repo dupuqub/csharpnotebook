c82cb712bf6d6dbfd84e2aceb918d7026121bf64
dupuqub <dupuqub@gmail.com>
2018-01-21 12:53:24 -0200

    Indentação

--- Script08.cs
+++ Script08.cs
@@ -4,52 +4,52 @@ using UnityEngine;
 
 public class Script08 : MonoBehaviour
 {
-  Vector2 Dadinho = new Vector2( 5, 6 );
-  Vector3 Acerola = new Vector3( 5, 6, 7 );
-  Vector3 PosicaoTemporaria;
+	Vector2 Dadinho = new Vector2( 5, 6 );
+	Vector3 Acerola = new Vector3( 5, 6, 7 );
+	Vector3 PosicaoTemporaria;
 
-  void Start()
-  {
-    // Vector2 e 3 são outros tipo de dado
-    // Vector2 só pode ter 2 valores (normalmente x e y)
-    // Vector3 só pode ter 3 valores (normalmente x, y e z)
-    //
-    Debug.Log( Dadinho ); // (5.0, 6.0)
-    Debug.Log( Acerola ); // (5.0, 6.0, 7.0)
+	void Start()
+	{
+		// Vector2 e 3 são outros tipo de dado
+		// Vector2 só pode ter 2 valores (normalmente x e y)
+		// Vector3 só pode ter 3 valores (normalmente x, y e z)
+		//
+		Debug.Log( Dadinho ); // (5.0, 6.0)
+		Debug.Log( Acerola ); // (5.0, 6.0, 7.0)
 
-    // Objetos podem ter seu elemento "transform" acessado
-    // ("lossyScale" é somente para leitura)
-    //
-    Debug.Log( "object     = " + transform.gameObject );
-    Debug.Log( "type       = " + transform.GetType() );
-    Debug.Log( "position   = " + transform.position );
-    Debug.Log( "rotation   = " + transform.rotation );
-    Debug.Log( "lossyScale = " + transform.lossyScale ); // Relativo ao mundo
-    Debug.Log( "localScale = " + transform.localScale ); // Relativo ao superior
+		// Objetos podem ter seu elemento "transform" acessado
+		// ("lossyScale" é somente para leitura)
+		//
+		Debug.Log( "object     = " + transform.gameObject );
+		Debug.Log( "type       = " + transform.GetType() );
+		Debug.Log( "position   = " + transform.position );
+		Debug.Log( "rotation   = " + transform.rotation );
+		Debug.Log( "lossyScale = " + transform.lossyScale ); // Relativo ao mundo
+		Debug.Log( "localScale = " + transform.localScale ); // Relativo ao superior
 
-    // Como "position" não pode ser diretamente alterado usa-se um "Vector3"
-    //
-    PosicaoTemporaria = transform.position;
+		// Como "position" não pode ser diretamente alterado usa-se um "Vector3"
+		//
+		PosicaoTemporaria = transform.position;
 
-    // O "Vector3" será moldado
-    //
-    PosicaoTemporaria.x -= 10;
-    PosicaoTemporaria.y -= 5;
+		// O "Vector3" será moldado
+		//
+		PosicaoTemporaria.x -= 10;
+		PosicaoTemporaria.y -= 5;
 
-    // E usado para reconstruir o "position" real
-    //
-    transform.position = PosicaoTemporaria;
-  }
+		// E usado para reconstruir o "position" real
+		//
+		transform.position = PosicaoTemporaria;
+	}
 
-  void Update()
-  {
-    // "deltaTime" é o tempo entre frames
-    //
-    var delta = Time.deltaTime;
+	void Update()
+	{
+		// "deltaTime" é o tempo entre frames
+		//
+		var delta = Time.deltaTime;
 
-    // Translate é um tipo de movimento que não precisa de "Vector3"
-    // Valores podem ser multiplicados pelo delta para maior suavidade
-    //
-    transform.Translate( 3 * delta, 0, 0 );
-  }
+		// Translate é um tipo de movimento que não precisa de "Vector3"
+		// Valores podem ser multiplicados pelo delta para maior suavidade
+		//
+		transform.Translate( 3 * delta, 0, 0 );
+	}
 }
--- Script09.cs
+++ Script09.cs
@@ -4,65 +4,65 @@ using UnityEngine;
 
 public class Script09 : MonoBehaviour
 {
-  // Essas variáveis podem ser modificadas no editor do Unity
-  //
-  public int      velocidade = 5;
-  public string[] controle   = { "a", "d", "w", "s" };
+	// Essas variáveis podem ser modificadas no editor do Unity
+	//
+	public float    velocidade = 5;
+	public string[] controle   = { "a", "d", "w", "s" };
 
-  // O movimento de uma peça será controlado pelo teclado
-  //
-  void Update()
-  {
-    // Antes de tudo eu declaro variáveis que serão usadas durante o processo
-    //
-    var delta    = Time.deltaTime;
-    var sentido  = new Dictionary< string, string >();
-    var novo_hor = "";
-    var novo_ver = "";
-    var novo_x   = 0;
-    var novo_y   = 0;
+	// O movimento de uma peça será controlado pelo teclado
+	//
+	void Update()
+	{
+		// Antes de tudo eu declaro variáveis que serão usadas durante o processo
+		//
+		var delta    = Time.deltaTime;
+		var sentido  = new Dictionary< string, string >();
+		var novo_hor = "";
+		var novo_ver = "";
+		var novo_x   = 0;
+		var novo_y   = 0;
 
-    // Rodam todos os testes referentes ao teclado
-    //
-    novo_hor =
-      Input.GetKey( controle[ 0 ] ) && !Input.GetKey( controle[ 1 ] )
-      ? "esquerda"
-      : Input.GetKey( controle[ 1 ] ) && !Input.GetKey( controle[ 0 ] )
-      ? "direita"
-      : "nada";
+		// Rodam todos os testes referentes ao teclado
+		//
+		novo_hor =
+			Input.GetKey( controle[ 0 ] ) && !Input.GetKey( controle[ 1 ] )
+			? "esquerda"
+			: Input.GetKey( controle[ 1 ] ) && !Input.GetKey( controle[ 0 ] )
+			? "direita"
+			: "nada";
 
-    novo_ver =
-      Input.GetKey( controle[ 2 ] ) && !Input.GetKey( controle[ 3 ] )
-      ? "cima"
-      : Input.GetKey( controle[ 3 ] ) && !Input.GetKey( controle[ 2 ] )
-      ? "baixo"
-      : "nada";
+		novo_ver =
+			Input.GetKey( controle[ 2 ] ) && !Input.GetKey( controle[ 3 ] )
+			? "cima"
+			: Input.GetKey( controle[ 3 ] ) && !Input.GetKey( controle[ 2 ] )
+			? "baixo"
+			: "nada";
 
-    // Dá-se valor ao sentido depois dos testes
-    //
-    sentido.Add( "horizontal", novo_hor );
-    sentido.Add( "vertical", novo_ver );
+		// Dá-se valor ao sentido depois dos testes
+		//
+		sentido.Add( "horizontal", novo_hor );
+		sentido.Add( "vertical", novo_ver );
 
-    // Aplica-se a velocidade atual em variáveis temporárias
-    //
-    if( sentido[ "horizontal" ] != "nada" )
-    {
-      novo_x =
-        sentido[ "horizontal" ] == "esquerda"
-        ? -velocidade
-        : velocidade;
-    }
+		// Aplica-se a velocidade atual em variáveis temporárias
+		//
+		if( sentido[ "horizontal" ] != "nada" )
+		{
+			novo_x =
+				sentido[ "horizontal" ] == "esquerda"
+				? -velocidade
+				: velocidade;
+		}
 
-    if( sentido[ "vertical" ] != "nada" )
-    {
-      novo_y =
-        sentido[ "vertical" ] == "cima"
-        ? velocidade
-        : -velocidade;
-    }
+		if( sentido[ "vertical" ] != "nada" )
+		{
+			novo_y =
+				sentido[ "vertical" ] == "baixo"
+				? -velocidade
+				: velocidade;
+		}
 
-    // E por último move-se o objeto
-    //
-    transform.Translate( novo_x * delta, novo_y * delta, 0 );
-  }
+		// E por último move-se o objeto
+		//
+		transform.Translate( novo_x * delta, novo_y * delta, 0 );
+	}
 }
